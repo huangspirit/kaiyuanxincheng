62204327502398fa08bbcb02942eebf3ccc50603
nuonuo <13403893904@163.com>
2019-08-13 09:32:35 +0800

    品牌

--- src/views/Brand/Brand.vue
+++ src/views/Brand/Brand.vue
@@ -23,14 +23,14 @@
       <div class="mask"></div>
     </div>
     <!-- 最新入驻 -->
-    <div class="new-settledin">
+    <div class="prodclass-r">
       <span>最新入驻</span>
-      <div v-if="imgList.length>0" class="swiper-container">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide" v-for="(item,index) in imgList" :key="index">
-            <img :src="item.imgurl" alt />
-          </div>
-        </div>
+      <div class="slideWrap" @mouseenter="handleEnter" @mouseleave="handleLeave">
+        <ul :class="{anim:animate==true}">
+          <li v-for="(item,k) in imgList" :key="k">
+            <ImgE :src="item.imgurl" :W="300" :H="100"></ImgE>
+          </li>
+        </ul>
       </div>
     </div>
     <!-- 全部品牌 -->
@@ -90,6 +90,7 @@ export default {
       findBrandList: [],
       findBrandListKey: [],
       imgList: [],
+      animate: false,
       siderListShow: false
     };
   },
@@ -97,7 +98,6 @@ export default {
   mounted() {
     this.getBrandList();
     window.addEventListener("scroll", this.handleScroll);
-    this.swriper();
   },
   destroyed() {
     window.removeEventListener("scroll", this.handleScroll); //  离开页面清除（移除）滚轮滚动事件
@@ -135,27 +135,33 @@ export default {
             this.listKey = this.findBrandListKey[0];
             this.imgList = [];
             this.imgList = this.findBrandList[this.listKey];
-            this.swriper();
           }
         });
     },
     send(val) {
       this.imgList = [];
       this.imgList = this.findBrandList[val];
-      console.log(this.imgList);
-      this.swriper();
       this.listKey = val;
       var _id = document.getElementById(val);
       window.scrollTo(0, _id.offsetTop - 65);
     },
-    swriper() {
-      var mySwiper = new Swiper(".swiper-container", {
-        speed: 3000,
-        autoplay: true,
-        slidesPerView: 6,
-        spaceBetween: 0,
-        loop: true
-      });
+    handleEnter() {
+      clearInterval(this.interval);
+    },
+    handleLeave() {
+      this.interval = setInterval(this.scroll, 5000);
+    },
+    scroll() {
+      console.log("gundong");
+      if (this.imgList.length > 6) {
+        this.animate = true; // 因为在消息向上滚动的时候需要添加css3过渡动画，所以这里需要设置true
+        setTimeout(() => {
+          //  这里直接使用了es6的箭头函数，省去了处理this指向偏移问题，代码也比之前简化了很多
+          this.imgList.push(this.imgList[0]); // 将数组的第一个元素添加到数组的
+          this.imgList.shift(); //删除数组的第一个元素
+          this.animate = false; // margin-top 为0 的时候取消过渡动画，实现无缝滚动
+        }, 500);
+      }
     }
   }
 };
